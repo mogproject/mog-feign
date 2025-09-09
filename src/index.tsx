23bba1541cd529afafbbc00e9ea30ee3e0042291
Yosuke Mizutani <mogproj@gmail.com>
2025-09-10 03:08:14 +0900

    Implement ContextProvider wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@atlaskit/css-reset';
 import AppProvider from '@atlaskit/app-provider';
-
-import DiscordUsers from './components/sections/DiscordUsers';
 import './i18n/config'; // Set up i18n.
 
-import './styles/table.css';
-import Header from './components/Header';
+import App from './App';
 
 // Workaround for Atlassian Design System.
 // @see https://community.developer.atlassian.com/t/console-warnings-when-feature-gate-can-not-be-evaluated/93025
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error: __CRITERION__ is injected at runtime by Atlassian tooling
 window.__CRITERION__ = { getFeatureFlagOverride: () => false };
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <AppProvider>
-    <Header />
-    <DiscordUsers />
+    <App />
   </AppProvider>
 );
