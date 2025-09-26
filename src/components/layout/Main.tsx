23417d442fb8b6c80af5354361189b0a83dbcbee
Yosuke Mizutani <mogproj@gmail.com>
2025-09-27 01:01:21 +0900

    Update layout

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const mainStyles = css({
+  flex: 1,
+});
 
 type MainProps = {
   children: React.ReactNode;
 };
 
 const Main: React.FC<MainProps> = (props) => {
-  return <div>{props.children}</div>;
+  return <div css={mainStyles}>{props.children}</div>;
 };
 
 export default Main;
