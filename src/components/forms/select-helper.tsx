80523ff0d6043978762fa8a7bb9453afb3712f7d
Yosuke Mizutani <mogproj@gmail.com>
2025-09-30 20:07:12 +0900

    Fix a bug in option values

--- src/components/forms/select-helper.tsx
+++ src/components/forms/select-helper.tsx
@@ -3,5 +3,5 @@ import { OptionType } from '@atlaskit/select';
 export const createOption = (label: string) =>
   ({
     label: label,
-    value: label.toLowerCase().replace(/\W/g, ''),
+    value: label.toLowerCase(),
   } as OptionType);
