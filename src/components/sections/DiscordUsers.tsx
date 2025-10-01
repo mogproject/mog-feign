e7560ad9f7d9ba2826f01de367f68e9b8aec789d
Yosuke Mizutani <mogproj@gmail.com>
2025-10-01 20:53:08 +0900

    Resolve all issues

--- src/components/sections/DiscordUsers.tsx
+++ src/components/sections/DiscordUsers.tsx
@@ -301,7 +301,14 @@ function DiscordUsers() {
             <Field<string> name="name" isRequired validate={(value) => validateName(value || '', -1, false)} defaultValue="">
               {({ fieldProps, error }) => (
                 <Box xcss={xcss({ width: `${layout.mainWidth >= 600 ? 144 : 120}px`, minHeight: '60px' })}>
-                  <TextField {...fieldProps} css={compactTextFieldStyles} placeholder={t('name_placeholder')} autoComplete="off" />
+                  <TextField
+                    {...fieldProps}
+                    css={compactTextFieldStyles}
+                    placeholder={t('name_placeholder')}
+                    autoComplete="off"
+                    aria-label="name"
+                    aria-labelledby=""
+                  />
                   <MessageWrapper>{error && <ErrorMessage>{error}</ErrorMessage>}</MessageWrapper>
                 </Box>
               )}
@@ -311,7 +318,13 @@ function DiscordUsers() {
             <Field<string> name="id" isRequired validate={(value) => validateId(value || '', -1, false)} defaultValue="">
               {({ fieldProps, error }) => (
                 <Box xcss={xcss({ width: `${layout.mainWidth >= 600 ? 172 : 124}px`, minHeight: '60px' })}>
-                  <TextField {...fieldProps} css={compactTextFieldStyles} placeholder={t('id_placeholder')} />
+                  <TextField
+                    {...fieldProps}
+                    css={compactTextFieldStyles}
+                    placeholder={t('id_placeholder')}
+                    aria-label="id"
+                    aria-labelledby=""
+                  />
                   <MessageWrapper>{error && <ErrorMessage>{error}</ErrorMessage>}</MessageWrapper>
                 </Box>
               )}
@@ -335,6 +348,8 @@ function DiscordUsers() {
                     autoFocus={false}
                     openMenuOnFocus={false}
                     menuPortalTarget={document.body}
+                    aria-label="groups"
+                    aria-labelledby=""
                   />
                 )}
               </Field>
